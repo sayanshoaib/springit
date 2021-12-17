15cef21302b4cc5fd570323aeb5629b6cfe2ac2b
Shoaib Hasan <Shoaib.Hasan1801@gmail.com>
2021-12-17 22:10:29 +0600

    Initial Commit for our Spring Boot Reddit Clone!

---
+++ src/main/java/com/vega/springit/SpringitApplication.java
@@ -0,0 +1,15 @@
+package com.vega.springit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class SpringitApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringitApplication.class, args);
+    }
+
+}
