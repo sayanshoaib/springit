638392f4087a0d88184f5143f56014b6177cf748
Shoaib Hasan <Shoaib.Hasan1801@gmail.com>
2021-12-19 18:05:48 +0600

    Created our entities and refactored using Project Lombok

--- src/main/java/com/vega/springit/SpringitApplication.java
+++ src/main/java/com/vega/springit/SpringitApplication.java
@@ -1,16 +1,18 @@
 package com.vega.springit;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class SpringitApplication {
 
-    public static void main(String[] args) {
+    private static final Logger log = LoggerFactory.getLogger(SpringitApplication.class);
 
+    public static void main(String[] args) {
         SpringApplication.run(SpringitApplication.class, args);
-        System.out.println("Welcome to Springit");
     }
 
 }
